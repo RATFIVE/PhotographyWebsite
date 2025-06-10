6b6f65dd3ce639bc53cf782b71312d578d5c9267
RATFIVE <generalpegasus@web.de>
2025-06-10 21:26:53 +0200

    feat: update asset paths in index.html for correct deployment on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PhotographyWebsite/', // Für GitHub Pages: Name des Repos mit / am Anfang und Ende
   plugins: [react()],
 })
